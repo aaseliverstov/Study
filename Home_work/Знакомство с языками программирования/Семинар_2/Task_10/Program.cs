e675684e0cf25a67fd030e73968a434de1ce1ff1
Alexey Seliverstov <>
2022-08-25 17:39:53 +1000

    добавил домашнюю работу по второму семестру

---
+++ Home_work/Знакомство с языками программирования/Семинар_2/Task_10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное 
+//число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int result = (num / 10) - ((num / 100) * 10);
+
+if (num > 99 && num < 1000)
+{
+    Console.WriteLine($"{num} -> {result}");
+}
+else if (num < -99 && num > -1000)
+{
+    result = result * -1;
+    Console.WriteLine($"{num} -> {result}");
+}
+else
+{
+    Console.WriteLine("Введено не корректное число");
+}
---
+++ Home_work/Знакомство с языками программирования/Семинар_2/Task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру (справа налево) 
+// заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 6
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+
+if (num > 99 || num < -99)
+{
+    int result = (((num / 1000) * 10) - (num / 100));
+    if (num > 0) result = result * -1;
+    Console.WriteLine(result);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+
+
+
