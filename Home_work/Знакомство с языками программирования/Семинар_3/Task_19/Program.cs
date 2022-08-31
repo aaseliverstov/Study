1062c6ad3f85c2bace1e979f497eae773d8efa49
Alexey Seliverstov <>
2022-08-31 18:34:12 +1000

    Добавил домашнюю работу для семинара 3

---
+++ Home_work/Знакомство с языками программирования/Семинар_3/Task_19/Program.cs
@@ -0,0 +1,43 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и 
+//проверяет, является ли оно палиндромом. Строки и массивы использовать 
+//нельзя!
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine());
+
+// Первое решение
+
+int x1 = number % 10;
+int x2 = (number / 10) % 10;
+int x3 = (number / 100) % 10;
+
+int result = ((((((((x1 * 10) + x2) * 10) + x3) * 10) + x2) *10) + x1);
+
+if(number == result)
+{
+    Console.WriteLine($"{number} -> да");
+}
+else
+{
+    Console.WriteLine($"{number} -> нет");
+}
+
+// Второе решение 
+
+// int x1 = (number / 10000);
+// int x2 = (number / 1000) % 10;
+// int x4 = (number / 10) % 10;
+// int x5 = number % 10;
+
+// if(x1 == x5 && x2 == x4)
+// {
+//     Console.WriteLine($"{number} -> да");
+// }
+// else
+// {
+//     Console.WriteLine($"{number} -> нет");
+// }
