58264fe4e3e33fda619948f235506045b84f65fe
Alexey Seliverstov <>
2022-09-09 00:33:49 +1000

    Добавил домашнее задание к семинару 5

---
+++ Home_work/Знакомство с языками программирования/Семинар_5/Task_36/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int NotEvenNumbers(int[] arr)
+{
+    int result = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        result = result + arr[i];
+    }
+    return result;
+}
+
+int[] myArray = GetArray(6, 1, 9);
+int num = NotEvenNumbers(myArray);
+Console.Write(string.Join(" ", myArray));
+Console.WriteLine($" -> {num}");
---
+++ Home_work/Знакомство с языками программирования/Семинар_5/Task_38/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным 
+// элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] GetDoubleArray(int size)
+{
+    double[] res = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().NextDouble();
+        res[i] = res[i] * 100;
+    }
+    return res;
+}
+
+double MinNumb(double[] arr, int size)
+{
+    double min = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (min < arr[i]);
+        else min = arr[i];
+    }
+    return min;
+}
+
+double MaxNumb(double[] arr, int size)
+{
+    double max = arr[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (max > arr[i]);
+        else max = arr[i];
+    }
+    return max;
+}
+
+double Difference (double max, double min)
+{
+    double difference;
+    difference = max - min;
+    return difference;
+}
+
+double[] myArray = GetDoubleArray(6);
+Console.WriteLine(string.Join(" ", myArray));
+
+double minNumber = MinNumb(myArray, 6);
+Console.WriteLine($"Минимальное значение: {minNumber}");
+
+double maxNumber = MaxNumb(myArray, 6);
+Console.WriteLine($"Максимальное значение: {maxNumber}");
+
+double diffNumber = Difference(maxNumber, minNumber);
+Console.WriteLine($"Разница: {diffNumber}");
