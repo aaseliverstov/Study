65b222f412c9681a561b8aea4896f6bd8cec8a9e
Alexey Seliverstov <>
2022-09-12 23:34:46 +1000

    Добавил домашнее задание к семинару 6

---
+++ Home_work/Знакомство с языками программирования/Семинар_6/Task_41/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел 
+// больше 0 ввёл пользователь(сначала пользователь вводит количество значений - M, 
+// далее M-раз сами значения).
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+int[] Numbers(int m)
+{
+    int[] numbers = new int[m];
+    int count;
+    for (int i = 0; i < m; i++)
+    {
+        count = i + 1;
+        Console.Write($"Введите {count} число: ");
+        numbers[i] = int.Parse(Console.ReadLine());
+    }
+    return numbers;
+}
+
+int PlusNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.Write("Введите количество значений: ");
+int M = int.Parse(Console.ReadLine());
+if (M > 0)
+{
+    Console.WriteLine($"Количество значение больше 0: {PlusNumber(Numbers(M))}");
+}
+else
+{
+    Console.WriteLine("Количество значений не может быть" +
+                               " отрицательным или равно нулю, введите корректное" +
+                               " значение.");
+}
+
+
---
+++ Home_work/Знакомство с языками программирования/Семинар_6/Task_43/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+double SearchX(double B1, double K1, double B2, double K2)
+{
+    double x = (B2 - B1) / (K1 - K2);
+    return x;
+}
+
+double SearchY(double x, double K1, double B1)
+{
+    double y = K1 * x + B1;
+    return y;
+}
+
+Console.Write("Введите значение b1: ");
+double b1 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение k1: ");
+double k1 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение b2: ");
+double b2 = int.Parse(Console.ReadLine());
+Console.Write("Введите значение k2: ");
+double k2 = int.Parse(Console.ReadLine());
+if (k1 == k2) Console.WriteLine("Введены одинаковые значения для k1 и k2," +
+                             " отрезки будут параллельны");
+else
+{
+    double x = SearchX(b1, k1, b2, k2);
+    Console.WriteLine($"({x}, {SearchY(x, k1, b1)})");
+}
+
