52e0fe012849dc4a09494a53471f898e2129ab35
Alexey Seliverstov <>
2022-09-19 05:23:17 +1000

    Добавил домашнее задание к семинару 9

---
+++ Home_work/Знакомство с языками программирования/Семинар_9/Task_66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая 
+// найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int NumberRec(int a, int b, int counter)
+{
+    if (b != counter) a += NumberRec(++a, --b, counter);
+    return a;
+}
+
+Console.Write("Введите число m: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (M > 0 && N > 0)
+{
+    int counter = M;
+    Console.WriteLine();
+    Console.WriteLine(NumberRec(M, N, counter));
+}
+else Console.WriteLine("Ведите значения больше 0");
---
+++ Home_work/Знакомство с языками программирования/Семинар_9/Task_68/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с 
+// помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m != 0) && (n == 0)) return AckermanFunction(m - 1, 1);
+    else return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+}
+Console.Write("Введите число m: ");
+int M = int.Parse(Console.ReadLine());
+Console.Write("Введите число n: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine(AckermanFunction(M, N));
