4bcdb492314ef493e7a3432d0c755ed3259da160
aaseliverstov <>
2024-01-21 23:07:03 +1000

    Add homework2

---
+++ Home_work/Spring/seminar2/src/main/java/com/example/seminar2/Application.java
@@ -0,0 +1,15 @@
+package com.example.seminar2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Используя Spring Boot, создайте простое веб-приложение, которое приветствует пользователя на главной странице.
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ Home_work/Spring/seminar2/src/main/java/com/example/seminar2/Service.java
@@ -0,0 +1,8 @@
+package com.example.seminar2;
+
+@org.springframework.stereotype.Service
+public class Service {
+    public String getGreeting(){
+        return "<h1 align=\"center\">Hello, User!</h1>";
+    }
+}
